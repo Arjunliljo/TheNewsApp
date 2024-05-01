37c8d2a31a78c00db75e13321f028e8105eedcfb
Arjun cv <arjun7180@gmail.com>
2024-05-01 19:27:41 +0530

    s

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const mainContent = document.querySelector(".grid__mainContent img");
+const mainH2 = document.querySelector(".grid__mainContent h2");
+const mainParagraph = document.querySelector(".grid__mainContent p");
+
 const subContentImg = document.querySelectorAll(".sub-contents img");
 const subContentH2 = document.querySelectorAll(".sub-contents H2");
 const subContentSpan = document.querySelectorAll(".sub-contents span");
+const subContentBtn = document.querySelectorAll(".sub-contents button");
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
@@ -16,23 +20,29 @@ async function collectData() {
 }
 
 (async () => {
+  const titleCorrecter = (title) => {
+    if (title.length >= 30) return title.slice(0, 40).concat(".".repeat(3));
+    else title;
+  };
+
   try {
     const data = await collectData();
     console.log(data);
 
     if (data.length > 0) {
       mainContent.src = data[0].image_url;
+      mainH2.innerHTML = titleCorrecter(data[0].title);
 
       subContentImg.forEach((el, i) => {
         if (data[i + 1]) {
-          if (data[i + 1].image_url) {
-            el.src = data[i + 1].image_url;
-          } else {
-            i++;
-            el.src = data[i + 1].image_url;
-          }
+          if (!data[i + 1].image_url)
+            el.src =
+              "https://cpworldgroup.com/wp-content/uploads/2021/01/placeholder.png";
+          else el.src = data[i + 1].image_url;
+
           subContentH2[i].innerHTML = data[i + 1].title;
           subContentSpan[i].innerHTML = data[i + 1].creator[0];
+          subContentBtn[i].innerHTML = data[i + 1].pubDate.split(" ")[0];
         } else {
           alert("Not enough articles to populate all elements.");
         }
